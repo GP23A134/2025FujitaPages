ab6eeaa9a0808c684a27bd1bb26074b98f878992
GP23A134 <gp23a134@oecu.jp>
2025-10-30 10:03:59 +0900

    Add files via upload

---
+++ KGS4WD/options.js
@@ -0,0 +1,95 @@
+/* 
+ * 詳細検索用の条件設定
+ * 　・"id"の値が重複しないように注意
+ *   ・データの数は必要に応じて増減してよい． 
+ */
+let search_cond = 
+[
+  {
+    "id": "cond1",
+    "ctext": " 上位クラス=カクテル",
+    "cond": "wdt:P279",
+    "val": "wd:Q134768",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond2",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond3",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond4",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond5",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond6",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  }
+];
+
+/* 
+ * 検索項目の設定
+ * 　・"id"の値が重複しないように注意
+ *   ・データの数は必要に応じて増減してよい． 
+ */
+let search_prop = 
+[
+  {
+    "id": "opt1",
+    "prop": "wdt:P186",
+    "pname": "材料",
+    "optional": true
+  },
+  {
+    "id": "opt2",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt3",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt4",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt5",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  }
+];
